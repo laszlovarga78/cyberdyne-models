57ab05e400950fcc565aed260625d17361e6340a
Laszlo Varga <laszlo.1.varga@gmail.com>
2022-02-21 19:47:11 +0100

    Terminator models with class based components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import TerminatorList from "./components/terminator-list/terminator-list.component";
+import SearchBox from "./components/searchbox/searchbox.component";
+
+import Scroll from "./components/scroll/scroll.component";
+import ErrorBoundary from "./components/error-boundary/error-boundary.component";
+
+import "./App.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      models: [],
+      searchfield: "",
+    };
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ models: users }));
+  }
+
+  render() {
+    const filteredModels = this.state.models.filter((model) => {
+      return model.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+
+    if (this.state.models.length === 0) {
+      return <h1>Betöltés folyamatban...</h1>;
+    }
+
+    return (
+      <div className="tc">
+        <h1 className="f1">Terminator modellek</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <ErrorBoundary>
+            <TerminatorList models={filteredModels} />
+          </ErrorBoundary>
+        </Scroll>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/terminator/terminator.component.jsx
@@ -0,0 +1,15 @@
+//import React from 'react';
+
+const Terminator = ({ id, name, serialNumber }) => {
+  return (
+    <div className="bg-light-blue dib br3 pa3 ma2 grow tc">
+      <img src={`https://robohash.org/${id}?size=180x180`} alt="terminator" />
+      <div>
+        <h2>{name}</h2>
+        <p>SN: {serialNumber}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Terminator;
